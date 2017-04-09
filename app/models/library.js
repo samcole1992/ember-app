1acad1a35b8de4a87c071b2494dc6b6eba398730
Samuel Cole <samcole1992@gmail.com>
2017-04-08 21:46:01 -0400

    cleaned up controllers and components

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  books: DS.hasMany('book'),
+
+  isValid: Ember.computed.and('name', 'address')
+
 });
